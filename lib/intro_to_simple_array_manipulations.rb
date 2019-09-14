8865668fb19e6eab5d24b9dd1d85878c3a651e7a
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-09-14 15:41:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,7 @@ using_pop(continents)
 continents.size
 
 def pop_with_args(array)
-array.pop
+array.pop.pop
 end
 dog_breeds = ["Chihuahua", "Shiba Inu","Great Dane", "Golden Retriever"]
 small_dogs = pop_with_args(dog_breeds)
